a15d39934c0ee45f8cfd0ac8977420566bf2059e
propgmachine <77746698+propgmachine@users.noreply.github.com>
2021-03-09 23:15:51 -0500

    Updated
    pwd
    cd

--- main.js
+++ main.js
@@ -1,13 +1,26 @@
 function numberDoubler(num) {
-    // Your code here
+    let outputNum = num;
+    while (outputNum < 100) {
+        outputNum = outputNum * 2;
+    }
+    return outputNum;
 }
 
 function stringRepeater(str) {
-    // Your code here
+    let outputStr = str;
+    while (outputStr.length < 10) {
+        outputStr += str;
+    } 
+    return outputStr;
 }
 
+
 function makeDivisible(x, y) {
-    // Your code here
+    let outputX = x;
+    while (outputX % y !== 0) {
+        outputX++;
+    }
+    return outputX;
 }
 
 module.exports = {
